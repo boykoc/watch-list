e4d5917d3ad85353164acb5cff77ef150f60d59b
Cody Boyko <codyboyko@yahoo.ca>
2017-08-10 13:37:19 +0000

    Setup google authentication.
    This is a mix of the richonrails and the google clould platform implementation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
+  # [START helper_methods]
+  helper_method :logged_in?, :current_user
+  
+  def logged_in?
+    session.has_key? :user
+  end
+  
+  def current_user
+    Marshal.load session[:user] if logged_in?
+  end
+  # [END helper_methods]
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
